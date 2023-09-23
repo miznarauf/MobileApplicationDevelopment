822b47b7a9bcc58f1dda233d80f35f2fcef54698
miznarauf <miznarauf19@gmail.com>
2023-09-23 17:44:25 +0500

    chapter6 practice

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:app01/pages/home.dart';
+import 'package:flutter/src/material/colors.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Starter Template',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home:const Home(),
+    );
+  }
+}
